79299498d74876f2ac3fe8075716b39a5bdd04cd
Michael Dahl <michael@sharewayz.de>
2017-09-28 18:24:48 +0200

    Versioning

--- projectile-trailblazer.el
+++ projectile-trailblazer.el
@@ -4,7 +4,7 @@
 
 ;; Author:            Michael Dahl <michael.dahl84@gmail.com>
 ;; URL:               https://github.com/micdahl/projectile-trailblazer
-;; Version:           0.1.0
+;; Version:           0.2.0
 ;; Keywords:          rails, projectile, trailblazer, languages
 ;; Package-Requires:  ((emacs "24.4") (projectile "0.12.0") (inflections "1.1") (inf-ruby "2.2.6") (f "0.13.0") (rake "0.3.2"))
 
